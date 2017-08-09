f02f3be525a6d41d7858f37ac874cc4505e8b508
Abhansh Giri <abhanshgiri21@gmail.com>
2017-08-09 21:00:09 +0530

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,161 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var multer = require('multer');
+var upload = multer({
+  dest: './uploads'
+});
+
+var connection = mysql.createConnection({
+  host    :'localhost',
+  user    :'root',
+  password:'root',
+  database:'portfolio'
+});
+
+connection.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * FROM projects', function(err, rows, fields){
+	  if(err) throw err;
+	  
+	  res.render('dashboard', { 
+		"rows" : rows,
+		layout: 'layout2' });
+  });
+  //start 4th video
+  
+});
+
+router.get('/new', function(req, res){
+  res.render('new');
+});
+
+router.post('/new',upload.single('projectimage'),  function(req, res){
+	var title       = req.body.title;
+	var description = req.body.description;
+	var service     = req.body.service;
+	var client      = req.body.client;
+	var projectdate = req.body.projectdate;
+
+
+	if(req.file){
+		var projectImageOriginalName = req.file.originalname;
+		var projectImageName = req.file.filename;
+		var projectImageMime = req.file.mimetype;
+		var projectImagePath = req.file.path;
+		var projectImageExt = req.file.extension;
+		var projectImageSize = req.file.size;
+	} else {
+		var projectImageName = 'noimage.jpg';
+	}
+	req.checkBody('title', 'Title field is required').notEmpty();
+	req.checkBody('service', 'Service field is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('new', {
+			errors: errors,
+			title: title,
+			description: description,
+			service: service,
+			client: client
+		});
+	} else {
+		var project = {
+			title: title,
+			description: description,
+			service: service,
+			client: client,
+			date: projectdate,
+			image: projectImageName
+		}
+
+
+		var query = connection.query('INSERT INTO projects SET ?', project, function(err, result){
+			//project inserted
+		});
+
+		req.flash('success', 'project saved sucesfully');
+		res.location('/admin');
+		res.redirect('/admin');
+	}
+});
+
+router.get('/edit/:id', function(req, res, next) {
+  connection.query('SELECT * FROM projects WHERE id = ' +req.params.id, function(err, row, fields){
+	  if(err) throw err;
+	  
+	  res.render('edit', { 
+		"row" : row[0],
+		layout: 'layout2' });
+  });
+  //start 4th video
+  
+});
+
+router.post('/edit/:id',upload.single('projectimage'),  function(req, res){
+	var title       = req.body.title;
+	var description = req.body.description;
+	var service     = req.body.service;
+	var client      = req.body.client;
+	var projectdate = req.body.projectdate;
+
+
+	if(req.file){
+		var projectImageOriginalName = req.file.originalname;
+		var projectImageName = req.file.filename;
+		var projectImageMime = req.file.mimetype;
+		var projectImagePath = req.file.path;
+		var projectImageExt = req.file.extension;
+		var projectImageSize = req.file.size;
+	} else {
+		var projectImageName = 'noimage.jpg';
+	}
+	req.checkBody('title', 'Title field is required').notEmpty();
+	req.checkBody('service', 'Service field is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('new', {
+			errors: errors,
+			title: title,
+			description: description,
+			service: service,
+			client: client
+		});
+	} else {
+		var project = {
+			title: title,
+			description: description,
+			service: service,
+			client: client,
+			date: projectdate,
+			image: projectImageName
+		}
+
+
+		var query = connection.query('UPDATE projects SET ? WHERE id ='+req.params.id , project, function(err, result){
+			//project inserted
+		});
+
+		req.flash('success', 'project updated sucesfully');
+		res.location('/admin');
+		res.redirect('/admin');
+	}
+});
+
+router.delete('/delete/:id', function(req, res){
+	connection.query('DELETE FROM projects WHERE id ='+ req.params.id, function(err, result){
+		if(err) throw err;
+	});
+	req.flash('success', 'project deleted');
+	res.location('/admin');
+	res.redirect('/admin');
+});
+
+
+module.exports = router;
